e57e84d8041083c89a454bc05b02ff7dc3fb41eb
Bhavya jain <bhavyajain3004@gmail.com>
2023-12-22 20:59:42 +0530

    Create classintro.java

---
+++ EasyLevel/classintro.java
@@ -0,0 +1,19 @@
+class Task{
+  int length;
+  int weight;
+  int height;
+
+public void set_length (int l){
+  length = l;
+}
+public void set_width (int w){
+  weight = w;
+}
+public void set_height (int h){
+   height = h;
+}
+public void volume() {
+  System.void.println (length*width*height);
+  }
+}
+  
